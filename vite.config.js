763d871de0cb460f6a5ebc2e6337cf592447d505
William Ma <58480359+williamma621@users.noreply.github.com>
2026-02-24 21:15:47 -0800

    added config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: ' https://github.com/williamma621/HRV-Research-Software.git',
   plugins: [react()],
 })
